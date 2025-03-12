270fb20e6c4467d0a799cd4014c64689f0579566
Felipe <luisfel3.0@gmail.com>
2025-03-12 11:54:43 -0500

    create json api tours

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/differentroads/api/Handler.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/differentroads/api/Handler.java
@@ -1,11 +1,15 @@
 package co.com.differentroads.api;
 
+import co.com.differentroads.model.tour.Tour;
 import co.com.differentroads.model.tour.gateways.TourRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
@@ -20,7 +24,11 @@ private  final TourRepository tourRepository;
 
     public Mono<ServerResponse> listenGETOtherUseCase(ServerRequest serverRequest) {
         return tourRepository.findById(4000)
-                .flatMap(tour -> ServerResponse.ok().bodyValue(tour));
+                //.flatMap(tour -> ServerResponse.ok().bodyValue(tour));
+                .flatMap(tour -> {
+                    Map<String, List<Tour>> response = Map.of("tours", List.of(tour));
+                    return ServerResponse.ok().bodyValue(response);
+                });
     }
 
     public Mono<ServerResponse> listenPOSTUseCase(ServerRequest serverRequest) {
